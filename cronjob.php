465fad5c1873d217aa094cf2eb4ae2cc112f1176
Sr. Developer <brunowebmaster@live.com>
2024-11-07 13:13:52 -0300

    m

--- cronjob.php
+++ cronjob.php
@@ -22,7 +22,7 @@ Arquivo de Log menor que 35MB
 ';
 }
 
-if(date('i') == 30 || date('i') == 5 || date('i') == 0) { 
+if(in_array(date('i'), [0,5,15,30,45])) { 
      echo '
 Checando e realizando atualizando do módulo
 ';
