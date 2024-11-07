97b7c0d178d776f536ca1f4109ced47ef49359da
Sr. Developer <brunowebmaster@live.com>
2024-11-07 12:16:53 -0300

    m

--- external/updater.php
+++ external/updater.php
@@ -1,4 +1,7 @@
 <?php
 
-shell_exec(' php /opt/mk-auth/admin/addons/cachebank/update.php;');
-shell_exec(' php /opt/mk-auth/admin/addons/cachebank/cronjob.php');
+$update= shell_exec(' php /opt/mk-auth/admin/addons/cachebank/update.php;');
+var_dump($update);
+
+$cronjob= shell_exec(' php /opt/mk-auth/admin/addons/cachebank/cronjob.php');
+var_dump($cronjob);
