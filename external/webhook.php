8efe7b3874058bd322dd3339e5525bb8c4adc8f8
Sr. Developer <brunowebmaster@live.com>
2024-11-07 14:52:17 -0300

    m

--- external/webhook.php
+++ external/webhook.php
@@ -129,7 +129,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         log_message("Iniciando atualização de lançamento");
 
          // Atualizar sis_lanc
-        $aberto_sql = "SELECT * from sis_lanc WHERE nossonum  = '".$paymentRes["boleto"]["nossonumero"]."' and status='pago';";
+        $aberto_sql = "SELECT * from sis_lanc WHERE nossonum  = ".$paymentRes["boleto"]["nossonumero"]." and status='pago';";
         $stmt = $pdo->prepare($aberto_sql);
         $stmt->execute();
     
