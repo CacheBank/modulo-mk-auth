817b7c9838842bafbb7e27c63dfba14a4e4c565b
Sr. Developer <brunowebmaster@live.com>
2024-11-07 14:52:54 -0300

    m

--- external/webhook.php
+++ external/webhook.php
@@ -139,8 +139,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             log_message("Aualizando2 lançamento usando nosso numero " . $paymentRes["boleto"]["nossonumero"]);
 
               // Atualizar sis_lanc
-              $aberto_sql = "SELECT * from sis_lanc WHERE nossonum  = '".$paymentRes["boleto"]["nossonumero"]."' and status='pago';";
-            
             $aberto_result = $conn->query($aberto_sql);
 
             while ($fatura = $aberto_result->fetch_assoc()) {
