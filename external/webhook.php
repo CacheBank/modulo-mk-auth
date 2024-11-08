c31e3d96b83bbde7e2eee23b499afa2f6f6d8163
Sr. Developer <brunowebmaster@live.com>
2024-11-07 21:20:38 -0300

    m

--- external/webhook.php
+++ external/webhook.php
@@ -83,6 +83,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $amountPaid=$paymentRes["status"]===7?$paymentRes["valortotal"]:$paymentRes["valorpago"];
         $statusName=getStatusPaymentName($paymentRes["status"]);
 
+        echo "Dados da transação
+        ";
+        print_r($paymentRes);
+        echo "
+        Fim dados da transação";
+
         $amount_fees=$paymentRes["custo"];
 
         log_message("Atribuindo valores ao lançamento " . $last_id);
