9045650ef4f2b14f4da1a77d92a30dde01cd3659
Sr. Developer <brunowebmaster@live.com>
2025-01-07 15:04:43 -0300

    m

--- external/cachebank-view-boleto.php
+++ external/cachebank-view-boleto.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
           $query = "SELECT cinvoices.id_lanc as id_lanc, cinvoices.idtransaction
                           FROM cachebank_invoices cinvoices
                             inner join sis_lanc sis_lanc on sis_lanc.id=cinvoices.id_lanc
-                          WHERE cinvoices.id_lanc = :id_lanc or sis_lanc.uuid_lanc = :id_lanc";
+                          WHERE sis_lanc.id = :id_lanc or sis_lanc.uuid_lanc = :id_lanc";
                 $stmt = $pdo->prepare($query);
                 if (!$stmt) {
                     throw new Exception("Erro ao preparar declaração SQL para selecionar de pix_info: " . $pdo->error);
