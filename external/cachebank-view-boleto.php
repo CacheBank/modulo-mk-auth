3723a3fcd2050862fd08d6bb2fbe6c09cea639ff
Sr. Developer <brunowebmaster@live.com>
2025-01-07 15:12:38 -0300

    m

--- external/cachebank-view-boleto.php
+++ external/cachebank-view-boleto.php
@@ -3,7 +3,7 @@ include '/opt/mk-auth/admin/addons/cachebank/db.hhvm';
  include '/opt/mk-auth/admin/addons/cachebank/includes/utils.hhvm';
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $idLanca=(int)$_GET["titulo"];
+        $idLanca=$_GET["titulo"];
 
           $query = "SELECT cinvoices.id_lanc as id_lanc, cinvoices.idtransaction
                           FROM cachebank_invoices cinvoices
@@ -13,7 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 if (!$stmt) {
                     throw new Exception("Erro ao preparar declaração SQL para selecionar de pix_info: " . $pdo->error);
                 }
-                $stmt->bindParam(":id_lanc", $idLanca,  PDO::PARAM_INT);
+                if(is_string($idLanca)){
+                    $stmt->bindParam(":id_lanc", $idLanca,  PDO::PARAM_INT);
+                }else {
+                    $stmt->bindParam(":id_lanc", $idLanca,  PDO::PARAM_STR);
+                }
                 $stmt->execute();
                 $resDb=$stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -23,27 +27,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                 };
                 $idtransaction=$resDb["idtransaction"];
                 header('Location: https://fatura.cachebank.com.br/show/boleto/'.$idtransaction);
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $idLanca=(int)$_POST["titulo"];
-
-      $query = "SELECT cinvoices.id_lanc as id_lanc, cinvoices.idtransaction
-                      FROM cachebank_invoices cinvoices
-                        inner join sis_lanc sis_lanc on sis_lanc.id=cinvoices.id_lanc
-                      WHERE sis_lanc.id = :id_lanc or sis_lanc.uuid_lanc = :id_lanc";
-            $stmt = $pdo->prepare($query);
-            if (!$stmt) {
-                throw new Exception("Erro ao preparar declaração SQL para selecionar de pix_info: " . $pdo->error);
-            }
-            $stmt->bindParam(":id_lanc", $idLanca,  PDO::PARAM_INT);
-            $stmt->execute();
-            $resDb=$stmt->fetch(PDO::FETCH_ASSOC);
-
-            if(empty($resDb["idtransaction"])){
-                header('Location: /boleto/boleto2.hhvm?titulo='.$idLanca);
-                return ;
-            };
-            $idtransaction=$resDb["idtransaction"];
-            header('Location: https://fatura.cachebank.com.br/show/boleto/'.$idtransaction);
 }
