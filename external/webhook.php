3d8af6df4b9cdb7f0279411ce2ed22c0366f9986
Sr. Developer <brunowebmaster@live.com>
2025-01-16 11:01:05 -0300

    obtendo dados do boleto pela referencia do boleto

--- external/webhook.php
+++ external/webhook.php
@@ -148,7 +148,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ";
 
         // Obtem dados do lançamento atual
-        $aberto_sql2 = "SELECT id, datapag, nossonum, valorpag, `login`, datapag,coletor,formapag from sis_lanc WHERE id = ".$id_lanc." and login = '".$login_cliente."' ";
+        $aberto_sql2 = "SELECT id, datapag, nossonum, valorpag, `login`, datapag, coletor,`status`, formapag from sis_lanc WHERE id = ".$id_lanc." and login = '".$login_cliente."' ";
         $aberto_result2 = $conn->query($aberto_sql2);
         while ($fatura = $aberto_result2->fetch_assoc()) {
             print_r($fatura);
@@ -175,7 +175,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 and login = '".$login_cliente."' 
                 and (
                     `status` != '".$statusName."'
-                    or datapag != DATE_FORMAT('".$dataFormatada."', '%Y-%m-%d %H:%i:%s')
                     or coletor != 'notificacao'
                     or valorpag != ".$amountPaid."
                     or formapag != 'dinheiro'
