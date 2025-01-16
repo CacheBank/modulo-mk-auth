e8063c9ea095c5eedd7901af5df1051e23ddd864
Sr. Developer <brunowebmaster@live.com>
2025-01-16 14:22:44 -0300

    obtendo dados do boleto pela referencia do boleto

---
+++ external/check_lanc.php
@@ -0,0 +1,49 @@
+<?php
+ include '/opt/mk-auth/admin/addons/cachebank/db.hhvm';
+ include '/opt/mk-auth/admin/addons/cachebank/includes/utils.hhvm';
+ include '/opt/mk-auth/admin/addons/cachebank/includes/client_v2_api.php';
+
+// Definir cabeçalhos de resposta para JSON
+header('Content-Type: application/json');
+
+  // Rota para conciliar cachebank x mk-auth
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $config = getConfig($pdo);
+  
+    $input = (string) file_get_contents('php://input');
+    $data = json_decode($input, true);
+
+        // Verificar se os dados são válidos
+        $client_id = isset($data['client_id']) ? trim($data['client_id']): '';
+        $client_secret = isset($data['client_secret']) ? trim($data['client_secret']): '';
+
+        $lancId = isset($data['lanc_id']) ? trim($data['lanc_id']): '';
+
+        $local_client_id=trim($config->client_id);
+        $local_client_secret=trim($config->client_secret);
+
+        if(!$client_id || $client_id==""){
+            log_message("Client ID diferente do recebido: " );
+            return ;
+        }
+        else if($local_client_id!=$client_id){
+            echo 'not found';
+            return ;
+        }else if($local_client_secret!=$client_secret){
+            echo 'not found';
+            return ;
+        }
+       
+        $aberto_sql2 = "SELECT id, datapag, nossonum, recibo, valorpag, `login`, datapag, coletor,`status`, formapag,num_recibos,referencia,datavenc,deltitulo from sis_lanc WHERE `id` = '".$lancId."' ";
+        $aberto_result2 = $conn->query($aberto_sql2);
+        $results = $aberto_result2->fetchAll(PDO::FETCH_ASSOC);
+        $json_data = json_encode($results);
+
+        echo $json_data;
+
+
+}
+
+$pdo=null;
+?>
+
